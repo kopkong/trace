22eddd3ca09416d59ca1417bce08b25db9167a8e
Xuan Wang <xuwang@nvidia.com>
2013-11-26 13:45:07 +0800

    11

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
-rm -r build
+export ANDROID_NDK=~/data/android-ndk-r8e/
+r m-r build
 cmake -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain/android.toolchain.cmake -DANDROID_API_LEVEL=9 -H. -Bbuild
 make -C build
